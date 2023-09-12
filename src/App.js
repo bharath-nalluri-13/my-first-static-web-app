ec310b4c0e991b49cbb30d86cafab4bbdd038ddd
bharath-nalluri-13 <113639532+bharath-nalluri-13@users.noreply.github.com>
2023-09-11 17:51:57 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>
-<p>I did my homework!</p>
-<img src="Lab1.png">
-</div>;
+  return (
+    <div>
+      <p>I did my homework!</p>
+      <img src="Lab1.png" alt="Lab1" />
+    </div>
+  );
 }
 
 export default App;
